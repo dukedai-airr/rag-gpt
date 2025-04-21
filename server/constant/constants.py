c9e149dfa3e14dcfc791f4fa1c4f002ef9e31d1d
Duke Dai <dan.dai@airrlabs.com>
2025-04-21 18:24:55 +0800

    feat: change embedding model

--- server/constant/constants.py
+++ server/constant/constants.py
@@ -23,7 +23,8 @@ OPENAI_EMBEDDING_MODEL_NAME = "text-embedding-3-small"
 ZHIPUAI_EMBEDDING_MODEL_NAME = "embedding-2"
 
 # Name of the Ollama model used for embedding text
-OLLAMA_EMBEDDING_MODEL_NAME = "mxbai-embed-large"
+# OLLAMA_EMBEDDING_MODEL_NAME = "mxbai-embed-large"
+OLLAMA_EMBEDDING_MODEL_NAME = "nomic-embed-text"
 
 # Maximum length of text chunks when splitting up large documents
 MAX_CHUNK_LENGTH = 1300
